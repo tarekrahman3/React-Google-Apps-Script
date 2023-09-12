517bb5ec6f4ce32c94647b55a0dcb5b801a15176
tarekrahman3 <md.tarek420@gmail.com>
2023-09-12 08:10:30 +0600

    a

--- src/client/dialog-demo-mui/App.js
+++ src/client/dialog-demo-mui/App.js
@@ -2,85 +2,90 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 
-const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    {
-        field: 'firstName',
-        headerName: 'First name',
-        width: 150,
-        editable: true,
-    },
-    {
-        field: 'lastName',
-        headerName: 'Last name',
-        width: 150,
-        editable: true,
-    },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 110,
-        editable: true,
-    },
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-];
+// const columns = [
+//     { field: 'id', headerName: 'ID', width: 90 },
+//     {
+//         field: 'firstName',
+//         headerName: 'First name',
+//         width: 150,
+//         editable: true,
+//     },
+//     {
+//         field: 'lastName',
+//         headerName: 'Last name',
+//         width: 150,
+//         editable: true,
+//     },
+//     {
+//         field: 'age',
+//         headerName: 'Age',
+//         type: 'number',
+//         width: 110,
+//         editable: true,
+//     },
+//     {
+//         field: 'fullName',
+//         headerName: 'Full name',
+//         description: 'This column has a value getter and is not sortable.',
+//         sortable: false,
+//         width: 160,
+//         valueGetter: (params) =>
+//             `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+//     },
+// ];
 
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+// const rows = [
+//     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+//     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+//     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+//     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+//     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+// ];
 
-//export default function DataGridDemo() {
-//    return (
-//        <Box sx={{ height: 400, width: '100%' }}>
-//            <DataGrid
-//                rows={rows}
-//                columns={columns}
-//                initialState={{
-//                    pagination: {
-//                        paginationModel: {
-//                            pageSize: 5,
-//                        },
-//                    },
-//                }}
-//                pageSizeOptions={[5]}
-//                checkboxSelection
-//                disableRowSelectionOnClick
-//            />
-//        </Box>
-//    );
-//}
 
-export default function DataTable() {
+// export default function DataTable() {
+//     return (
+//         <div style={{ height: 800, width: '100%' }}>
+//             <DataGrid
+//                 rows={rows}
+//                 columns={columns}
+//                 initialState={{
+//                     pagination: {
+//                         paginationModel: { page: 0, pageSize: 5 },
+//                     },
+//                 }}
+//                 pageSizeOptions={[5, 10]}
+//                 checkboxSelection
+//             />
+//         </div>
+//     );
+// }
+function App() {
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+      // Replace 'your-apps-script-web-app-url' with the actual URL of your deployed web app
+      fetch('https://script.google.com/macros/s/your-apps-script-web-app-url/exec')
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data from Google Apps Script:', error);
+        });
+    }, []);
+  
+    const columns = []; // Define your column configuration here
+  
     return (
-        <div style={{ height: 800, width: '100%' }}>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                initialState={{
-                    pagination: {
-                        paginationModel: { page: 0, pageSize: 5 },
-                    },
-                }}
-                pageSizeOptions={[5, 10]}
-                checkboxSelection
-            />
-        </div>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid rows={data} columns={columns} />
+      </div>
     );
-}
+  }
+  
+  export default App;
--- src/server/sheets.js
+++ src/server/sheets.js
@@ -29,3 +29,16 @@ export const setActiveSheet = (sheetName) => {
   SpreadsheetApp.getActive().getSheetByName(sheetName).activate();
   return getSheetsData();
 };
+
+['Status', 'Dealer', 'Entry Time (UTC)', 'Taken Out Time (UTC)', 'Stayed For (Days)', 'Product_URL', 'Brand', 'Title and Model', 'Year', 'KM', 'Price', 'Fuel_type', 'Horse_power', 'Transmission', 'Color', 'Marker']
+export const getAllTakenOutCars = () => {
+  const takenOutCars = [];
+  const rows = SpreadsheetApp.getActive().getSheetByName(sheetName).getDataRange().getValues()
+  for (const row of rows) {
+    if (row[0]=="Taken Out"){
+       takenOutCars.push({
+        column1:row[0]
+       })
+    }
+  }
+}
